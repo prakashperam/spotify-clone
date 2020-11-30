b5c840bda14a7bbf93ef8775344632ac5b226a9b
prakashperam <prakashperam123@gmail.com>
2020-11-30 17:47:41 +0530

    logo and login button styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login'
+
+function App() {
+  return (
+    <div className="App">
+      i m a clone
+      <Login />
+     
+    </div>
+  );
+}
+
+export default App;
